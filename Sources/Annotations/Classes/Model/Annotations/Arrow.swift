4880f48a36c2258cf6a29dea6a8001b2aab61f8d
Yulian Drapaylo <ulian.onua@gmail.com>
2023-02-27 19:49:25 +0100

    Add a logic to resize lineWidth and parts of Array when canvas is being resized

--- Sources/Annotations/Classes/Model/Annotations/Arrow.swift
+++ Sources/Annotations/Classes/Model/Annotations/Arrow.swift
@@ -1,10 +1,12 @@
 import Foundation
 import CoreGraphics
 
-public struct Arrow: TwoPointsModel, Figure {
+public struct Arrow: TwoPointsModel, Figure, Sizeable {
   public var id: String = UUID().uuidString
   public var color: ModelColor = .zero
   public var zPosition: CGFloat = 0
+  
+  public var lineWidth: CGFloat = 5.0
   
   public var origin: AnnotationPoint
   public var to: AnnotationPoint
