2f52b0f0f37f94b84f3eaab7c279dabb30cba2c8
Yulian Drapaylo <ulian.onua@gmail.com>
2023-11-30 15:07:02 +0100

    Update the rectangle selection path to be around rectangle borders + 15 points margin

--- Sources/Annotations/Classes/Model/Selections/SelectionPath/Certain/RectSelectionPathCreator.swift
+++ Sources/Annotations/Classes/Model/Selections/SelectionPath/Certain/RectSelectionPathCreator.swift
@@ -3,6 +3,16 @@ import CoreGraphics
 
 final class RectSelectionPathCreator: SelectionPathCreator {
   func createSelectionPath(for annotation: Rect) -> CGPath {
-    RectPathCreator().createPath(for: annotation)
+    let path = RectPathCreator().createPath(for: annotation)
+      
+    if annotation.rectType == .regular {
+      // lineWidth + 15 points around for the rectangle selection path
+      return path.copy(strokingWithWidth: annotation.lineWidth + 15.0,
+                       lineCap: .butt,
+                       lineJoin: .miter,
+                       miterLimit: 1)
+    } else {
+      return path
+    }
   }
 }
